c7cd1608e369a7653a8ad94a6ed4fd5ea0911cf1
Tanya <tanika@mit.edu>
2018-10-27 11:47:20 -0700

    scripts

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/tanya/computer_vision/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/tanya/computer_vision/catkin_ws/build/usb_cam/cmake_install.cmake")
   include("/home/tanya/computer_vision/catkin_ws/build/object_recognition/cmake_install.cmake")
 
 endif()
